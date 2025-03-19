c45d07470801e7be61561d1e38783aa95e8e0faf
zistro <zistro@proton.me>
2025-03-19 21:12:50 +0600

    Update net.sh

--- dwmblocks scripts/net.sh
+++ dwmblocks scripts/net.sh
@@ -6,3 +6,11 @@ else
     echo "INTERRUPTED"
 fi
 
+# Uncomment this part when using wifi and comment uper if to fi part
+# you can also change the ethernet nerd font icon from  to   just
+# edit blocks.def.h and rm blocks.h and sudo make install
+# if nmcli -t -f TYPE,STATE device | grep -q "^wifi:connected$"; then
+#    wifi_status="CONNECTED"
+# else
+#    wifi_status="INTERRUPTED"
+# fi
